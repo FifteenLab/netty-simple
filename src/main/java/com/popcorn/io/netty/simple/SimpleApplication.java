96e1fca6205c2d8c563de6420b554e0866859d6f
liubing <191360017@qq.com>
2020-04-14 16:42:17 +0800

    Revert "first commit"
    This reverts commit b037dddf

--- src/main/java/com/popcorn/io/netty/simple/SimpleApplication.java
+++
@@ -1,9 +0,0 @@
-package com.popcorn.io.netty.simple;
-
-import io.netty.util.NettyRuntime;
-
-public class SimpleApplication {
-    public static void main(String[] args) {
-        System.out.println(NettyRuntime.availableProcessors());
-    }
-}
